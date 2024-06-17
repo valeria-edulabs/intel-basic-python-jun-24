712c47a9dbf3e1c4ffe5583dae6462c68e6b9974
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-06-17 15:29:04 +0300

    additions

---
+++ meeting1/fruits.py
@@ -0,0 +1,37 @@
+fruits = [
+    "apple",
+    "banana",
+    "orange",
+    "grape",
+    "watermelon",
+    "pineapple",
+    "mango",
+    "strawberry",
+    "blueberry",
+    "raspberry",
+    "blackberry",
+    "cherry",
+    "pear",
+    "kiwi",
+    "lemon",
+]
+
+# for fruit in fruits:
+#     print(f"Fruit {fruit}: {len(fruit)}" )
+
+# for char in "BANANA":
+#     print(char)
+# #
+# for elem in ["a", "b", "c"]:
+#     print(elem)
+
+# word = input("Insert a word: ")
+# while word != "$":
+# # while "$" not in word:
+#     word = input("Insert a word: ")
+
+while True:
+    word = input("Insert a word: ")
+    if word == "$":
+        break
+print("Bye")
