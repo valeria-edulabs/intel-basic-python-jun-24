a22c2ab23180e9b4c65a2e59e2ceef71f778097b
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-06-24 14:11:20 +0300

    additions

--- meeting2/israel_id.py
+++ meeting2/israel_id.py
@@ -1,5 +1,15 @@
 def calc_control_digit(digits: str):
-    pass
+    total = 0
+    weights = [1,2,1,2,1,2,1,2]
+    for w, d in zip(weights, digits):
+        mul = w * int(d)
+        div, mod = divmod(mul, 10)
+        mul = div + mod
+        total += mul
+        # total += sum(divmod(w * int(d), 10))
+    return (10 - (total % 10)) % 10
 
-def calc_control_digit(digits: list[int]):
-    pass
+control_dig = calc_control_digit("11111111")
+print(control_dig)
+# def calc_control_digit(digits: list[int]):
+#     pass
